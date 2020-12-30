b117dae33c35f0b25b2fde3d6d30d87704afff45
Roland Salloum <roland.salloum00@outlook.com>
2020-12-30 17:04:51 +0200

    chore: added initial service structure

---
+++ item-rating/src/main/java/com/market/itemrating/ItemRatingApplication.java
@@ -0,0 +1,13 @@
+package com.market.itemrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemRatingApplication.class, args);
+    }
+
+}
---
+++ item-rating/src/test/java/com/market/itemrating/ItemRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.itemrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
