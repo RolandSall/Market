7832cf955ab3f71c1e7b01c92a6ce07a214ab3cb
Roland Salloum <roland.salloum00@outlook.com>
2021-01-10 21:06:58 +0200

    feat: added docker file for microservices for docker-compose.yml

--- basket/src/main/java/com/market/basket/BasketApplication.java
+++ basket/src/main/java/com/market/basket/BasketApplication.java
@@ -2,17 +2,15 @@ package com.market.basket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
-@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class BasketApplication {
 
     public static void main(String[] args) {
